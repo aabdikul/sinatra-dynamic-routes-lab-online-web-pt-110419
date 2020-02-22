2e632068f3336cc7c8e4de8a6ad9162e9e67897b
Aida Abdikulova <abdikulova@gmail.com>
2020-02-22 21:54:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     if opp = "add"
-      num1 + num2
+      "#{num1} + #{num2}"
     elsif opp = "subtract"
       num1 - num2
     elsif opp = "multiply"
