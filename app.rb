16f86599095d06e45544532c71ed953743abf80f
Aida Abdikulova <abdikulova@gmail.com>
2020-02-22 21:52:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,6 +28,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    opp = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     num1 + num2
